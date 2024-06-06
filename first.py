c46fe4b88639965f0ecb982f5988889255fbb4f2
Jatin Verma <22BAI71116@cuchd.in>
2024-06-07 01:17:56 +0530

    remove all files from repo

--- first.py
+++
@@ -1,4 +0,0 @@
-print('hello')
-a = "jatin"
-b = "hola"
-print(a+b)
--- iofile.py
+++
@@ -1,5 +0,0 @@
-with open('abc.txt','w') as file:
-    file.write('hello')
-  
-with open('abc.txt','r') as fol:
-    print(fol.read())
