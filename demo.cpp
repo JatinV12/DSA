804757db552451b1a193e0199a8fe0b7ebc47cee
Jatin Verma <22BAI71116@cuchd.in>
2024-06-15 21:03:12 +0530

    Adding my first dsa program

---
+++ demo.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"my first cpp program for DSA"<<endl;
+    cout<<"enter any number"<<endl;
+    int number;
+    cin>>number;
+    cout<<2*number;
+    return 0;
+}
